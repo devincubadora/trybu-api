c2dd42cd9c4c47b6755d53e45b85eed14a678c24
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-16 01:11:55 +0100

    feat: user validation

--- src/infra/http/dto/create-user-body.ts
+++ src/infra/http/dto/create-user-body.ts
@@ -1,7 +1,21 @@
+import { IsNotEmpty, Length, IsEmail } from 'class-validator';
+
 export class CreateUserBody {
+  @IsNotEmpty()
+  @Length(5, 50)
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  username: string;
+
+  @IsNotEmpty()
+  @Length(6, 20)
   password: string;
+
   phone?: string;
+
   whatsapp?: string;
 }
