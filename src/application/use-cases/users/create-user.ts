7dd674b57231efa4cb9f9cae1a2002b704fc5c26
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-25 17:03:43 +0100

    refactor: improve create user

--- src/application/use-cases/users/create-user.ts
+++ src/application/use-cases/users/create-user.ts
@@ -3,10 +3,16 @@ import { User } from '../../entities/user';
 import { UserRepository } from '../../repositories/user-repository';
 import { CreateUserBody } from '../../../infra/http/dto/create-user-body';
 import { Crypto } from '../../../utils/crypto';
+import { RecordAlreadyExistsError } from '../errors';
 
 interface CreateUserResponse {
   user: User;
 }
+
+type ValidationResponse = {
+  result: boolean;
+  message: string;
+};
 
 @Injectable()
 export class CreateUser {
@@ -14,8 +20,12 @@ export class CreateUser {
   async execute(request: CreateUserBody): Promise<CreateUserResponse> {
     const { name, username, email, password, phone, whatsapp } = request;
 
-    const hashedPassword = await Crypto.encrypt(password);
+    const validation = await this.validate(request);
 
+    if (!validation.result) {
+      throw new RecordAlreadyExistsError(validation.message);
+    }
+    const hashedPassword = await Crypto.encrypt(password);
     const user = await this.userRepository.create(
       new User({
         name,
@@ -26,7 +36,39 @@ export class CreateUser {
         whatsapp,
       }),
     );
+    return { user };
+  }
 
-    return { user } as any;
+  private async validate({
+    email,
+    phone,
+    whatsapp,
+    username,
+  }: CreateUserBody): Promise<ValidationResponse> {
+    const response = { result: true, message: '' };
+
+    const userAlredyExists = await this.userRepository.findMany({
+      where: {
+        OR: [{ email }, { phone }, { username }, { whatsapp }],
+      },
+    });
+
+    if (userAlredyExists.length > 0) {
+      response.result = false;
+      const user = userAlredyExists[0];
+
+      if (email == user.email) {
+        response.message = 'Este email já está registado.';
+      } else if (phone == user.phone) {
+        response.message = 'Este telefone já está registado.';
+      } else if (whatsapp == user.whatsapp) {
+        response.message = 'Este whatsapp já está registado.';
+      } else if (username == user.username) {
+        response.message = 'Este nome de usuário já está registado.';
+      } else {
+        console.log('user notf else', user);
+      }
+    }
+    return response;
   }
 }
