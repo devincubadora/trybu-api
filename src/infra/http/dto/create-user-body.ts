2dd35f12c11e4f367d99095ba7b5d22e6816b6bd
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-25 17:38:24 +0100

    refactor: user dto

--- src/infra/http/dto/create-user-body.ts
+++ src/infra/http/dto/create-user-body.ts
@@ -1,18 +1,30 @@
-import { IsNotEmpty, Length, IsEmail } from 'class-validator';
+import {
+  IsNotEmpty,
+  MinLength,
+  MaxLength,
+  IsEmail,
+  ValidateIf,
+} from 'class-validator';
 
 export class CreateUserBody {
-  @IsNotEmpty()
-  @Length(5, 50)
+  @MaxLength(50, { message: 'Nome muito longo' })
+  @MinLength(5, { message: 'Nome muito curto' })
+  @IsNotEmpty({ message: 'Informe o nome' })
   name: string;
 
-  @IsNotEmpty()
-  @IsEmail()
+  @IsEmail({}, { message: 'O email informado não á válido' })
+  @IsNotEmpty({ message: 'Informe o email' })
   email: string;
 
+  @MaxLength(20, { message: 'O nome de usuário muito longo' })
+  @MinLength(5, { message: 'O nome de usuário muito curto' })
+  @IsNotEmpty({ message: 'NOm' })
+  @ValidateIf((o) => o.username !== undefined && o.username !== null)
   username: string;
 
-  @IsNotEmpty()
-  @Length(6, 20)
+  @MaxLength(50, { message: 'Essa senha é muito longa' })
+  @MinLength(6, { message: 'A senha deve ter no mínimo 6 caracteres' })
+  @IsNotEmpty({ message: 'Infrome a senha' })
   password: string;
 
   phone?: string;
