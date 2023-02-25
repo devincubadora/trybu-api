bedd0c96ac6a3194516b5b90ede00f1224e8af75
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-25 17:35:59 +0100

    refactor: update providers imports

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,9 +3,8 @@ import { JwtService } from '@nestjs/jwt';
 import { AuthLogin } from '../../application/use-cases/auth/auth-login';
 import { CreateUser } from '../../application/use-cases/users/create-user';
 import { DeleteUser } from '../../application/use-cases/users/delete-user';
-import { FindOneUserByEmail } from '../../application/use-cases/users/find-one-user-by-email';
-import { FindOneUserById } from '../../application/use-cases/users/find-one-user-by-id';
-import { GetUsers } from '../../application/use-cases/users/get-users';
+import { FindOneUser } from '../../application/use-cases/users/find-one-user';
+import { FindManyUsers } from '../../application/use-cases/users/find-many-users';
 import { AtStarategy } from '../auth/jwt/at.strategy';
 import { DatabaseModule } from '../database/database.module';
 import { AuthController } from './controllers/auth.controller';
@@ -16,11 +15,11 @@ import { UsersContoller } from './controllers/user.controller';
   controllers: [UsersContoller, AuthController],
   providers: [
     CreateUser,
-    GetUsers,
+    FindManyUsers,
     DeleteUser,
     AuthLogin,
-    FindOneUserByEmail,
-    FindOneUserById,
+    FindOneUser,
+    FindOneUser,
     JwtService,
     AtStarategy,
   ],
