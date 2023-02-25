def5d6a8c147d812b7c97eae0c174776065ae47f
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-25 19:00:45 +0100

    feat: CountUsersParams

--- src/types/users-params.ts
+++ src/types/users-params.ts
@@ -11,6 +11,16 @@ export interface ManyUsersParams {
   cursor?: Prisma.UserWhereUniqueInput;
 }
 
+export interface CountUsersParams {
+  where?: Prisma.UserWhereInput;
+  orderBy?: Prisma.UserOrderByWithRelationInput;
+  select?: Prisma.UserCountAggregateInputType;
+  skip?: number;
+  take?: number;
+  distinct?: Prisma.Enumerable<Prisma.UserScalarFieldEnum>;
+  cursor?: Prisma.UserWhereUniqueInput;
+}
+
 export type OneUserParam = Prisma.UserWhereUniqueInput;
 
 export type DeleteOneUserParam = Prisma.UserWhereUniqueInput;
