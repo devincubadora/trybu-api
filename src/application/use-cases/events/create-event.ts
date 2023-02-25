4af2d0cc80d4938dc05b5485352027b0c6188503
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-26 00:27:00 +0100

    feat: create event use case

---
+++ src/application/use-cases/events/create-event.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@nestjs/common';
+import { EventService } from 'src/event/event.service';
+import { PrismaEventMapper } from 'src/infra/database/prisma/mappers/prisma-event-mapper';
+import { Event } from '../../entities/event';
+import { RecordAlreadyExistsError } from '../errors';
+
+interface CreateEventResponse {
+  event: Event;
+}
+
+type ValidationResponse = {
+  result: boolean;
+  message: string;
+};
+
+@Injectable()
+export class CreateEvent {
+  constructor(private eventRespository: EventService) {}
+
+  async execute(event: Event): Promise<CreateEventResponse> {
+    const validator = await this.validator(event);
+
+    if (!validator.result) {
+      throw new RecordAlreadyExistsError(validator.message);
+    }
+
+    const newEvent = await this.eventRespository.create(event);
+
+    return { event: PrismaEventMapper.toDomain(newEvent) };
+  }
+
+  private async validator({
+    title,
+    authorId,
+    startsAt,
+    address,
+  }: Event): Promise<ValidationResponse> {
+    const response = { result: true, message: '' };
+
+    const eventAlreadyExists = await this.eventRespository.findMany({
+      where: {
+        title,
+        authorId,
+        startsAt,
+        address,
+      },
+    });
+    if (eventAlreadyExists.length > 0) {
+      response.result = false;
+      response.message = 'Este evento já foi adicionado por si.';
+    }
+    return response;
+  }
+}
