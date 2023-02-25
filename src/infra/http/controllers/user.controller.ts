df8c9057c64a6eaa7415c43183ceaf3fad0f5c3c
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-25 17:36:28 +0100

    refactor: user controllers

--- src/infra/http/controllers/user.controller.ts
+++ src/infra/http/controllers/user.controller.ts
@@ -9,22 +9,23 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { CreateUser } from '../../../application/use-cases/users/create-user';
-import { GetUsers } from '../../../application/use-cases/users/get-users';
+import { FindManyUsers } from '../../../application/use-cases/users/find-many-users';
 import { DeleteUser } from '../../../application/use-cases/users/delete-user';
 import { CreateUserBody } from '../dto/create-user-body';
 import { UserViewModel } from '../view-model/user-view-model';
 import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
-import { FindOneUserById } from '../../../application/use-cases/users/find-one-user-by-id';
+import { FindOneUser } from '../../../application/use-cases/users/find-one-user';
+import { User } from 'src/application/entities/user';
 
 @UseGuards(AuthGuard('jwt'))
 @Controller('users')
 export class UsersContoller {
   constructor(
     private createUser: CreateUser,
-    private getUsers: GetUsers,
+    private findMany: FindManyUsers,
     private deleteUser: DeleteUser,
-    private findUserById: FindOneUserById,
+    private findOneUser: FindOneUser,
   ) {}
 
   @Post()
@@ -45,21 +46,22 @@ export class UsersContoller {
 
   @Get()
   async index() {
-    const { users } = await this.getUsers.execute();
+    const { users } = await this.findMany.execute();
 
     return { users: users.map(UserViewModel.toHTTP) };
   }
 
   @Get('me')
   async me(@Req() req: Request) {
-    const user = req.user;
-    const logeedUser = await this.findUserById.execute(user['sub']);
+    const user = req.user as User;
+    const logeedUser = await this.findOneUser.execute({ id: user.id });
+
     return UserViewModel.toHTTP(logeedUser);
   }
 
   @Delete(':userId')
   async deletes(@Param('userId') userId: string) {
-    const { result, message } = await this.deleteUser.execute(userId);
+    const { result, message } = await this.deleteUser.execute({ id: userId });
     return { result, message };
   }
 }
