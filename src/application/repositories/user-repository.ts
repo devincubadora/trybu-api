b6312b701183b9c0798fb65aaa5d77249ac955ae
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-25 18:58:44 +0100

    feat: add count method

--- src/application/repositories/user-repository.ts
+++ src/application/repositories/user-repository.ts
@@ -1,4 +1,5 @@
 import {
+  CountUsersParams,
   DeletedManyRecodsResponse,
   DeleteManyUserParam,
   DeleteOneUserParam,
@@ -10,10 +11,17 @@ import { User } from '../entities/user';
 
 export abstract class UserRepository {
   abstract create(user: User): Promise<User | null>;
+
   abstract findOne(param: OneUserParam): Promise<User | null>;
+
   abstract findMany(params?: ManyUsersParams): Promise<User[]>;
+
+  abstract count(params?: CountUsersParams): Promise<number>;
+
   abstract update(params: UpdateUserParam): Promise<User | null>;
+
   abstract delete(param: DeleteOneUserParam): Promise<User>;
+
   abstract deleteMany(
     param: DeleteManyUserParam,
   ): Promise<DeletedManyRecodsResponse>;
