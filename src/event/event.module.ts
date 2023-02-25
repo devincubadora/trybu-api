accebb9d44dfe38565d900b147c2ba8cebe94b87
Samuel Freitas <samuelfreitas.ao@gmail.com>
2023-02-26 00:29:59 +0100

    refactor: provide event usecases in module

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { CreateEvent } from 'src/application/use-cases/events/create-event';
+import { FindManyEvents } from 'src/application/use-cases/events/find-many-events';
 import { DatabaseModule } from '../infra/database/database.module';
 import { PrismaService } from '../infra/database/prisma/prisma.service';
 import { EventController } from './event.controller';
@@ -7,7 +9,7 @@ import { EventService } from './event.service';
 @Module({
   imports: [DatabaseModule],
   controllers: [EventController],
-  providers: [PrismaService, EventService],
-  exports: [EventService],
+  providers: [PrismaService, CreateEvent, FindManyEvents, EventService],
+  exports: [CreateEvent, FindManyEvents, EventService],
 })
 export class EventModule {}
